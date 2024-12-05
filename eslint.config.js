5faa7044788dccf451e491aa2725f98da9f2dcec
Roman Kurnovskii <r.kurnovskii@gmail.com>
2024-12-05 12:26:18 +0200

    refactor: eslint 9 support

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,38 @@
+import typescriptParser from '@typescript-eslint/parser';
+import typescriptPlugin from '@typescript-eslint/eslint-plugin';
+
 export default [
     {
-        ignores: ["src/types/global.d.ts", "node_modules", "dist"]
+        ignores: ['node_modules/**', 'dist/**'],
     },
     {
+        files: ['**/*.ts', '**/*.tsx'],
         languageOptions: {
-            parser: "@typescript-eslint/parser",
+            parser: typescriptParser,
             parserOptions: {
-                ecmaVersion: "latest",
-                sourceType: "module",
+                ecmaVersion: 2021,
+                sourceType: 'module',
+            },
+            globals: {
+                console: 'readonly',
+                require: 'readonly',
+                module: 'readonly',
+                __dirname: 'readonly',
+                Buffer: 'readonly',
+                process: 'readonly',
             },
         },
         plugins: {
-            "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+            '@typescript-eslint': typescriptPlugin, // Add TypeScript-specific linting
         },
         rules: {
-            semi: ["error", "always"],
-            quotes: ["error", "single"],
-            "@typescript-eslint/explicit-module-boundary-types": "off",
-            "@typescript-eslint/no-explicit-any": "off",
-            "@typescript-eslint/no-unused-vars": [
-                "error",
-                { argsIgnorePattern: "^_" },
+            semi: ['error', 'always'],
+            quotes: ['error', 'single'],
+            '@typescript-eslint/explicit-module-boundary-types': 'off',
+            '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                { argsIgnorePattern: '^_' },
             ],
         },
     },
